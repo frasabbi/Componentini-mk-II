37051abb0672493114a3d4088c8002146b419b64
frasabbi <fra.sabbi@gmail.com>
2019-04-11 17:59:06 +0200

    è solo l'inizio

--- Expansion-Panel/src/app/app.module.ts
+++ Expansion-Panel/src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExpansionComponent } from './expansion/expansion.component';
+import { PanelComponent } from './panel/panel.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      ExpansionComponent,
+      PanelComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
