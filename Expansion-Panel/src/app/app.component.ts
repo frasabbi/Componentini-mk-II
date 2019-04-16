07302ef4c3b0e8d0b87d4c63919a86ab7ae5c3ea
frasabbi <fra.sabbi@gmail.com>
2019-04-16 15:45:51 +0200

    Expansion panel
    Expansion Panel con un minimo di observable

--- Expansion-Panel/src/app/app.component.ts
+++ Expansion-Panel/src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { ExpansionComponent } from "./expansion/expansion.component";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  view:boolean;
-  onPanelView(isOpen:boolean){
-    this.view=isOpen;
+export class AppComponent implements OnInit{
+  view: boolean;
+
+  @ViewChild('expansion') expansionPanel: ExpansionComponent
+   ngOnInit(){
+    console.log(this.expansionPanel);
+    this.expansionPanel.onPanelView.subscribe((value) =>{
+      console.log('sottoscritto all\'output di expansion', value );
+    });
+  }
+  onPanelView(isOpen: boolean) {
+    this.view = isOpen;
   }
 }
